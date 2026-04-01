4371251e5929738bb69d24cbefe2a58e1fdd39e6
Niko Hujanen <niko@louhosdigital.fi>
2026-04-01 18:49:50 +0300

    feat: call + test

--- src/functions.php
+++ src/functions.php
@@ -9,3 +9,13 @@ function yoink(object $instance, string $property): mixed
 {
     return (fn () => $this->$property)->call($instance);
 }
+
+/**
+ * Call a private or protected method on an object.
+ *
+ * @param  mixed  ...$args
+ */
+function call(object $instance, string $method, ...$args): mixed
+{
+    return (fn () => $this->$method(...$args))->call($instance);
+}
--- tests/Unit/functionsTest.php
+++ tests/Unit/functionsTest.php
@@ -1,12 +1,25 @@
 <?php
 
+use function Louhos\Xpl\call;
 use function Louhos\Xpl\yoink;
 
-test('yoink works', function () {
+test('yoink', function () {
     $object = new class
     {
         private string $foo = 'bar'; // @phpstan-ignore property.onlyWritten
     };
 
     expect(yoink($object, 'foo'))->toBe('bar');
+});
+
+test('call', function () {
+    $object = new class
+    {
+        private function foo(string $bar): string // @phpstan-ignore method.unused
+        {
+            return $bar;
+        }
+    };
+
+    expect(call($object, 'foo', 'baz'))->toBe('baz');
 });
